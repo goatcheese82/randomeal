0f56433618f242530240755f4c8292d1e7fcac40
goatcheese82 <nonsquishy@gmail.com>
2017-11-22 18:11:07 -0600

    added CLI menu

---
+++ lib/randomeal/cli.rb
@@ -0,0 +1,42 @@
+class Randomeal::CLI
+
+    def call
+        puts "Thank you for using Randomeal!  Your options are loading."
+        Randomeal::Food.scrape
+        menu
+    end
+
+    def menu
+        puts <<-MENU
+
+        What would you like to eat?
+
+        1. Chicken
+        2. Burgers
+        3. Pasta
+        4. Salad
+        5. Dessert
+        MENU
+
+        input = gets.strip.downcase
+
+        if input == "1" || "chicken"
+            chicken
+        elsif input == "2" || "burgers"
+            burgers
+        elsif input == "3" || "pasta"
+            pasta
+        elsif input == "4" || "salad"
+            salad
+        elsif input == "5" || "desert"
+            dessert
+        else puts "That selection is not valid, please select from the list or 'exit'"
+        end
+
+    end
+
+    def chicken
+        
+
+
+end
