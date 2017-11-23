57a8be8e837b1a0e3a60b916b1cbd740bf8759ef
goatcheese82 <nonsquishy@gmail.com>
2017-11-22 18:18:13 -0600

    Added categories

--- lib/randomeal/food.rb
+++ lib/randomeal/food.rb
@@ -1,5 +1,10 @@
 class Randomeal::Food
 
-    attr_accessor :ingredients, :title, :directions
+    attr_accessor :ingredients, :title, :directions, :food
 
-    
+    @@food = []
+    @@categories = ["Chicken", "Burgers", "Pasta", "Salad", "Dessert"]
+
+    def self.scrape
+    end
+end
