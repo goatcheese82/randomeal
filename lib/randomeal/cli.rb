2bc17ecc378c94c7e5bdc002a617b17eedfbd095
goatcheese82 <nonsquishy@gmail.com>
2017-11-24 15:44:18 -0600

    final  commit

--- lib/randomeal/cli.rb
+++ lib/randomeal/cli.rb
@@ -5,13 +5,14 @@ class Randomeal::CLI
     def call
         puts "Thank you for using Randomeal!  Your options are loading."
         options
-        menu
         secondary_menu_options
         secondary_menu
     end
 
     def options
         puts <<-MENU
+
+        "-*-*-*-*-*-*-*-*-*"
 
         What would you like to eat?
 
@@ -21,13 +22,13 @@ class Randomeal::CLI
         4. Salad
         5. Dessert
         MENU
-
+        menu
     end
 
     def menu
 
         input = gets.strip.downcase
-
+        puts "-*-*-*-*-*-*-*-*-*"
         if input == "1" || input == "chicken"
             @selection = Randomeal::Food.new("chicken")
         elsif input == "2" || input == "burgers"
@@ -39,37 +40,44 @@ class Randomeal::CLI
         elsif input == "5" || input == "dessert"
             @selection = Randomeal::Food.new("dessert")            
         else puts "That selection is not valid, please select from the list or 'exit'"
+            options
         end
 
     end
 
     def secondary_menu_options
         puts <<-SECONDARY
+        
         Your meal is #{@selection.title}
         1. For ingredients
         2. For directions
         3. If you don't want to eat #{@selection.title}
-        'exit' to exit
+        
+        Type 'exit' to  quit.
         SECONDARY
     end
 
 
     def secondary_menu
+
+        input = nil
+
+        while input != "exit"
         input = gets.strip.downcase
-
-        if input == "1"
-            puts @selection.ingredients
-        elsif input == "2"
-            puts @selection.directions
-        elsif input == "3"
-            self.menu
-        else
-            puts "please make a selection"
+            if input == "1"
+                puts @selection.ingredients
+            elsif input == "2"
+                puts @selection.directions
+            elsif input == "3"
+                self.options
+            else
+                puts "Please make a valid selection or 'exit"
+            end
+            puts ""
+            puts "-*-*-*-*-*-*-*-*-*"
+            secondary_menu_options
+            puts "-*-*-*-*-*-*-*-*-*"
         end
-
-        puts "Would you like to make another selection?"
     end
-
-
 
 end
